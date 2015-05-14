2988ec1cb3d638ab2eb37ca8536539e624097b29
Jürn Brodersen <juern@uni-bremen.de>
2015-05-14 16:41:20 +0200

    Don't load json paralel

--- static/js/load_index_charts.js
+++ static/js/load_index_charts.js
@@ -1,7 +1,17 @@
-window.onload = function(){
-    new_graph("temp", false, '_type=hours&number=24');
-    new_graph("humi", false, '_type=hours&number=24');
-    new_graph("wind", false, '_type=hours&number=24');
-    new_graph("pres_rain", false, '_type=hours&number=24');
-    new_graph("wind_d_avg", false, '_type=hours&number=24');
-}
+new_graph("temp", false, '_type=hours&number=24')
+next = 'pres_rain'
+$( document ).ajaxComplete(function() {
+    if (next=='pres_rain'){
+        new_graph("pres_rain", false, '_type=hours&number=24')
+        next = 'humi'
+    }else if (next=='humi'){
+        new_graph("humi", false, '_type=hours&number=24')
+        next = 'wind'
+    }else if (next=='wind'){
+        new_graph("wind", false, '_type=hours&number=24')
+        next = 'wind_d_avg'
+    }else if (next=='wind_d_avg'){
+        new_graph("wind", false, '_type=hours&number=24')
+        next = 'None'
+    }
+});
--- static/js/load_stats_charts.js
+++ static/js/load_stats_charts.js
@@ -1,7 +1,17 @@
-window.onload = function(){
-    new_graph("temp", true, '_type=milliseconds&number=' + Date.now());
-    new_graph("humi", true, '_type=milliseconds&number=' + Date.now());
-    new_graph("wind", true, '_type=milliseconds&number=' + Date.now());
-    new_graph("pres_rain", true, '_type=milliseconds&number=' + Date.now());
-    new_graph("wind_d_avg", false, '_type=milliseconds&number=' + Date.now());
-}
+new_graph("temp", true, '_type=milliseconds&number=' + Date.now());
+next = 'pres_rain';
+$( document ).ajaxComplete(function() {
+    if (next=='pres_rain'){
+        new_graph("humi", true, '_type=milliseconds&number=' + Date.now());
+        next = 'humi';
+    }else if (next=='humi'){
+        new_graph("wind", true, '_type=milliseconds&number=' + Date.now());
+        next = 'wind';
+    }else if (next=='wind'){
+        new_graph("pres_rain", true, '_type=milliseconds&number=' + Date.now());
+        next = 'wind_d_avg';
+    }else if (next=='wind_d_avg'){
+        new_graph("wind_d_avg", false, '_type=milliseconds&number=' + Date.now());
+        next = 'None';
+    };
+});
